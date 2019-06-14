b4a8a7916dbc511a1b6ece3bc267a3f2c71e7983
Gary Tempus <gary@heysparkbox.com>
2019-06-14 19:56:34 -0400

    :art: refactor: Remove dead code.

--- src/commands/create.js
+++ src/commands/create.js
@@ -24,95 +24,7 @@ const path = require('path'),
       lambdaCode = require('../tasks/lambda-code'),
       initEnvVarsFromOptions = require('../util/init-env-vars-from-options'),
       NullLogger = require('../util/null-logger'),
-      OptionsValidatorExt = require('./options-validator');
-
-class OptionsValidator {
-  constructor(source, options, configFile, policyFiles) {
-    this.source = source;
-    this.options = options;
-    this.configFile = configFile;
-    this.policyFiles = policyFiles;
-    this.errorMessage = null;
-  }
-
-  validationError() {
-    if (this.source === os.tmpdir()) {
-      return 'Source directory is the Node temp directory. Cowardly refusing to fill up disk with recursive copy.';
-    }
-    if (!this.options.region) {
-      return 'AWS region is missing. please specify with --region';
-    }
-    if (this.options['optional-dependencies'] === false && this.options['use-local-dependencies']) {
-      return 'incompatible arguments --use-local-dependencies and --no-optional-dependencies';
-    }
-    if (!this.options.handler && !this.options['api-module']) {
-      return 'Lambda handler is missing. please specify with --handler';
-    }
-    if (this.options.handler && this.options['api-module']) {
-      return 'incompatible arguments: cannot specify handler and api-module at the same time.';
-    }
-    if (!this.options.handler && this.options['deploy-proxy-api']) {
-      return 'deploy-proxy-api requires a handler. please specify with --handler';
-    }
-    if (!this.options['security-group-ids'] && this.options['subnet-ids']) {
-      return 'VPC access requires at least one security group id *and* one subnet id';
-    }
-    if (this.options['security-group-ids'] && !this.options['subnet-ids']) {
-      return 'VPC access requires at least one security group id *and* one subnet id';
-    }
-    if (this.options.handler && this.options.handler.indexOf('.') < 0) {
-      return 'Lambda handler function not specified. Please specify with --handler module.function';
-    }
-    if (this.options['api-module'] && this.options['api-module'].indexOf('.') >= 0) {
-      return 'API module must be a module name, without the file extension or function name';
-    }
-    if (!fsUtil.isDir(path.dirname(this.configFile))) {
-      return 'cannot write to ' + this.configFile;
-    }
-    if (fsUtil.fileExists(this.configFile)) {
-      if (this.options && this.options.config) {
-        return this.options.config + ' already exists';
-      }
-      return 'claudia.json already exists in the source folder';
-    }
-    if (!fsUtil.fileExists(path.join(this.source, 'package.json'))) {
-      return 'package.json does not exist in the source folder';
-    }
-    if (this.options.policies && !this.policyFiles().length) {
-      return 'no files match additional policies (' + this.options.policies + ')';
-    }
-    if (this.options.memory || this.options.memory === 0) {
-      if (this.options.memory < limits.LAMBDA.MEMORY.MIN) {
-        return `the memory value provided must be greater than or equal to ${limits.LAMBDA.MEMORY.MIN}`;
-      }
-      if (this.options.memory > limits.LAMBDA.MEMORY.MAX) {
-        return `the memory value provided must be less than or equal to ${limits.LAMBDA.MEMORY.MAX}`;
-      }
-      if (this.options.memory % 64 !== 0) {
-        return 'the memory value provided must be a multiple of 64';
-      }
-    }
-    if (this.options.timeout || this.options.timeout === 0) {
-      if (this.options.timeout < 1) {
-        return 'the timeout value provided must be greater than or equal to 1';
-      }
-      if (this.options.timeout > 900) {
-        return 'the timeout value provided must be less than or equal to 900';
-      }
-    }
-    if (this.options['allow-recursion'] && this.options.role && isRoleArn(this.options.role)) {
-      return 'incompatible arguments allow-recursion and role. When specifying a role ARN, Claudia does not patch IAM policies.';
-    }
-  };
-
-  checkForValidationErrors() {
-    this.errorMessage = this.validationError();
-  };
-
-  validationErrorsExist() {
-    return !!this.errorMessage;
-  };
-}
+      OptionsValidator = require('./options-validator');
 
 module.exports = function create(options, optionalLogger) {
   'use strict';
@@ -348,7 +260,7 @@ module.exports = function create(options, optionalLogger) {
 	  return result;
 	};
 
-  const validator = new OptionsValidatorExt(source, options, configFile, policyFiles);
+  const validator = new OptionsValidator(source, options, configFile, policyFiles);
   validator.checkForValidationErrors();
   if (validator.validationErrorsExist()) return Promise.reject(validator.errorMessage);
 
